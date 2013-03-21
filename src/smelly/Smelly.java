9dd2625742e6180fcf83b521517efe567796c6bd
Eddy <bravogit@gmail.com>
2013-03-21 20:41:21 +0100

    File ordinato.

--- src/smelly/Smelly.java
+++ src/smelly/Smelly.java
@@ -1,38 +1,38 @@
 package smelly;
 
+import java.util.Collections;
+
 /**
- * Nell'orribile programma seguente dovrete operare come segue
- * 1) Creare opportuni test Junit
- * 2) Rifattorizzare (modificando il main di conseguenza e indicando che "puzza"
- * avete rilevato e che metodo avete scelto per risolverlo)
- * 3) Aggiungere gli opportuni Javadoc se non banali
+ * Nell'orribile programma seguente dovrete operare come segue 1) Creare
+ * opportuni test Junit 2) Rifattorizzare (modificando il main di conseguenza e
+ * indicando che "puzza" avete rilevato e che metodo avete scelto per
+ * risolverlo) 3) Aggiungere gli opportuni Javadoc se non banali
+ *
  * @author piffy
  */
 public class Smelly {
+
     public static void main(String[] args) {
-        Solid c = new Solid();
-        c.H=2.0f;
-        c.r=7.f;
-        c.type=1;
-        c.calcola();
-        System.out.println("Area di superficie:"+c.A);
-        System.out.println("Volume: "+c.V);
-        Solid d = new Solid();
-        d.r=5.0f;
-        d.H=2.0f;
-        d.type=1;
-        d.calcola();
-        Solid e = new Solid();
-        e.r=5.3f;
-        e.type=2;
-        e.calcola();
-        Solid Birillo[] = new Solid[10];
-        Birillo[1]=c;Birillo[2]=d;Birillo[3]=e;
-        if (Birillo[1].V>=Birillo[2].V && Birillo[1].V>=Birillo[3].V) 
+        Cilindro c = new Cilindro();
+        c.setAltezza(2.0f);
+        c.setRaggio(7.0f);
+        System.out.println("Area di superficie:" + c.getArea());
+        System.out.println("Volume: " + c.getVolume());
+        Cilindro d = new Cilindro();
+        d.setRaggio(5.0f);
+        d.setAltezza(2.0f);
+        Sfera e = new Sfera();
+        e.setRaggio(5.3f);
+        FiguraSolida Birillo[] = new FiguraSolida[10];
+        Birillo[1] = c;
+        Birillo[2] = d;
+        Birillo[3] = e;
+        if (Birillo[1].getVolume() >= Birillo[2].getVolume() && Birillo[1].getVolume() >= Birillo[3].getVolume()) {
             System.out.println("Il solido con volume maggiore è al posto 1");
-        else if (Birillo[2].V>=Birillo[1].V && Birillo[2].V>=Birillo[3].V) 
+        } else if (Birillo[2].getVolume() >= Birillo[1].getVolume() && Birillo[2].getVolume() >= Birillo[3].getVolume()) {
             System.out.println("Il solido con volume maggiore è al posto 2");
-        else if (Birillo[3].V>=Birillo[1].V && Birillo[3].V>=Birillo[2].V) 
+        } else if (Birillo[3].getVolume() >= Birillo[1].getVolume() && Birillo[3].getVolume() >= Birillo[2].getVolume()) {
             System.out.println("Il solido con volume maggiore è al posto 3");
+        }
     }
 }
