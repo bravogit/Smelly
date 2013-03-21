dc31f58fc0118c10011e98bc9889ec109a484aba
Eddy <bravogit@gmail.com>
2013-03-21 20:41:48 +0100

    Aggiunto il Comparable.

--- src/smelly/Sfera.java
+++ src/smelly/Sfera.java
@@ -8,7 +8,7 @@ package smelly;
  *
  * @author Eddy
  */
-public class Sfera implements FiguraSolida {
+public class Sfera implements FiguraSolida, Comparable {
 
     private float raggio;
 
@@ -22,12 +22,22 @@ public class Sfera implements FiguraSolida {
 
     @Override
     public float getArea() {
-        return (float) (4*Math.PI*raggio*raggio);
+        return (float) (4 * Math.PI * raggio * raggio);
     }
 
     @Override
     public float getVolume() {
-        return (float) (4/3*raggio*raggio*raggio*Math.PI);
+        return (float) (4 / 3 * raggio * raggio * raggio * Math.PI);
     }
-        
+
+    @Override
+    public int compareTo(Object t) {
+        try {
+            return (int) (((FiguraSolida) (t)).getVolume() - getVolume());
+        } catch (Exception e) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        } finally {
+            return 0;
+        }
+    }
 }
